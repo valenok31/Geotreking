ab3a50efe8e03cfbb442988d785858114319c543
Andrey <priv1984@yandex.ru>
2023-06-14 00:20:18 +0700

    preparation and setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-Geotreking
-    </div>
-  );
+    let latitude;
+    let longitude;
+
+    if ("geolocation" in navigator) {
+        /* местоположение доступно */
+        console.log(true)
+        console.log(navigator.geolocation)
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log(position.coords.latitude, position.coords.longitude);
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
+
+
+        });
+    } else {
+        /* местоположение НЕ доступно */
+        console.log(false)
+        console.log(navigator.geolocation)
+    }
+
+    return (
+        <div className="App">
+            Geotreking
+
+            <Geo />
+
+
+        </div>
+    );
 }
 
 export default App;
+
+function Geo (){
+    return <>
+
+
+    </>
+}
