dd7060d8286317282e1d4cf0569edf34aeeae846
Andrey <priv1984@yandex.ru>
2023-06-19 01:18:24 +0700

    Canvas

--- src/components/Canvas/Canvas.js
+++ src/components/Canvas/Canvas.js
@@ -11,29 +11,45 @@ export default function Canvas(props) {
         }
         let topPrevious = ((arrPoints[index - 1].latitude - props.getCoords[0].latitude) * 1000000 * 2);
         let leftPrevious = ((arrPoints[index - 1].longitude - props.getCoords[0].longitude) * 1000000 * 2);
-        if ((top - topPrevious) === 0 || (left - leftPrevious) === 0) {
+        let x = (left - leftPrevious);
+        let y = (top - topPrevious);
+        if (x === 0 || y === 0) {
             return <div className={s.point} style={{top: top - 5 + 'px', left: left - 5 + 'px'}}></div>
         }
-        let width = Math.sqrt((top - topPrevious) ** 2 + (left - leftPrevious) ** 2)
-
-        let rotation = ((top - topPrevious)>0 && (left - leftPrevious)>0 ? 90 : 270) - (Math.atan((leftPrevious - left) / (topPrevious - top)) * 180 / Math.PI);
-         //rotation = 0;
-        console.log(rotation)
+        let width = Math.sqrt((top - topPrevious) ** 2 + (left - leftPrevious) ** 2);
+        let widthHome = Math.sqrt((0 - top) ** 2 + (0 - left) ** 2);
+        let reg = (y > 0 ? 90 : 270);
+        let rotation = reg - (Math.atan(x / y) * 180 / Math.PI);
+        let rotationHome = reg - (Math.atan(-left / -top) * 180 / Math.PI);
+        console.log(Math.atan(x / y))
         return <>
-            <div className={s.point} style={{top: top - 5 + 'px', left: left - 5 + 'px'}}>{index}</div>
+            <div className={s.point} style={{top: top - 5 + 'px', left: left - 5 + 'px'}}></div>
             <div className={s.track} style={{
                 top: topPrevious + 'px',
                 left: leftPrevious + 'px',
                 width: width,
                 transform: `rotate(${rotation}deg)`
             }}></div>
+
         </>
 
     });
+    let lengthArr = props.getCoords.length
+    let top = ((props.getCoords[lengthArr-1].latitude - props.getCoords[0].latitude) * 1000000 * 2)
+    let left = ((props.getCoords[lengthArr-1].longitude - props.getCoords[0].longitude) * 1000000 * 2)
+    let widthHome = Math.sqrt((0 - top) ** 2 + (0 - left) ** 2);
+    let reg = (-top > 0 ? 90 : 270);
+    let rotationHome = reg - (Math.atan(-left / -top) * 180 / Math.PI);
 
     return <>
         <div className={s.canvas}>
             {canvasArr}
+            <div className={s.track} style={{
+                top: top + 'px',
+                left: left + 'px',
+                width: widthHome,
+                transform: `rotate(${rotationHome}deg)`
+            }}></div>
         </div>
     </>
 }
